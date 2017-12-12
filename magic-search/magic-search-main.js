a4642b315a3dd631ba5636dd75a723d66e20e70c
Mormegil <mormegil@centrum.cz>
2017-12-12 17:36:22 +0100

    Try load-on-demand to improve performance when not used

---
+++ magic-search/magic-search-main.js
@@ -0,0 +1,427 @@
+/* global jQuery */
+
+(function ($) {
+    var PROP_INSTANCE_OF = 'P31';
+    var RuleType = Object.freeze({
+        PRESENT: 1,
+        NOT_PRESENT: 2,
+        EQUAL: 3,
+        NOT_EQUAL: 4,
+        ANY_OF: 5,
+        ALL_OF: 6,
+        NONE_OF: 7
+        // TODO: DIFFERENT_THAN, DIFFERENT_THAN_ALL_OF
+    });
+    var i18n = {
+        cs: {
+            portletlink: 'Hledat podle této',
+            portlettooltip: 'Hledání dalších položek na základě této položky',
+            heading: 'Hledat podle této',
+            searchbtn: 'Hledat',
+            sparqlbtn: 'SPARQL',
+            clearbtn: 'Smazat vše',
+            clearconfirm: 'Opravdu smazat vše?',
+            remove: '❌',
+
+            type1: 'je definováno',
+            type2: 'není definováno',
+            type3: 'je rovno',
+            type4: 'není rovno',
+            type5: 'je libovolné z',
+            type6: 'je všechno z',
+            type7: 'je žádné z',
+        },
+        en: {
+            portletlink: 'Search by this',
+            portlettooltip: 'Search for other items based on this one',
+            heading: 'Search by this',
+            searchbtn: 'Search',
+            sparqlbtn: 'SPARQL',
+            clearbtn: 'Clear all',
+            clearconfirm: 'Really clear all?',
+            remove: '❌',
+
+            type1: 'is present',
+            type2: 'is not present',
+            type3: 'is equal to',
+            type4: 'is not equal to',
+            type5: 'is any of',
+            type6: 'is all of',
+            type7: 'is none of',
+        }
+    };
+    var lang = $('html').attr('lang') || 'en';
+    var msgs = i18n[lang] || i18n['en'];
+
+    var model = [];
+    var $uibox = null;
+    var $uirulelist = null;
+    var $searchbtn = null;
+    var $sparqlbtn = null;
+    var $clearbtn = null;
+
+    reinstallFull();
+
+    function reinstallFull() {
+        var $portletLink = $('#sbt-portletlink');
+        $portletLink.click(function (e) {
+            e.preventDefault();
+            displayUiBox();
+        });
+
+        $('#sbt-uibox').remove();
+        displayUiBox();
+    }
+
+    function displayUiBox() {
+        if ($uibox) return;
+
+        $uibox = $('<div id="sbt-uibox">');
+        $uibox.append($('<a href="#" class="sbt-close">').text('x').click(closeUibox));
+        $uibox.append($('<h3>').text(msgs.heading));
+        $uirulelist = $('<ul class="sbt-rulelist">');
+        $uibox.append($uirulelist);
+        $searchbtn = $('<button>').text(msgs.searchbtn);
+        $searchbtn.click(executeSearch);
+        $uibox.append($searchbtn);
+        $sparqlbtn = $('<button>').text(msgs.sparqlbtn);
+        $sparqlbtn.click(editSparql);
+        $uibox.append($sparqlbtn);
+        $clearbtn = $('<button>').text(msgs.clearbtn);
+        $clearbtn.click(clearModel);
+        $uibox.append($clearbtn);
+
+        rerenderUiRules();
+        $(document.body).append($uibox);
+
+        $('.wikibase-statementgroupview-property-label').append($('<button class="sbt-btn">+</button>').click(addProperty));
+        $('.wikibase-statementview-mainsnak .wikibase-snakview-variation-valuesnak a[href^="/wiki/Q"]').after($('<button class="sbt-btn">+</button>').click(addItemValue));
+        // TODO: .wikibase-snakview-variation-novaluesnak, .wikibase-snakview-variation-somevaluesnak
+    }
+
+    function clearModel() {
+        if (model.length === 0) return false;
+        if (!confirm(msgs.clearconfirm)) return false;
+        model = [];
+        rerenderUiRules();
+    }
+
+    function closeUibox() {
+        $uibox.remove();
+        $uibox = null;
+        $('.sbt-btn').remove();
+        $uirulelist = null;
+        $searchbtn = null;
+        $sparqlbtn = null;
+        $clearbtn = null;
+    }
+
+    function addProperty() {
+        var $button = $(this);
+        var $link = $button.parent().children('a').first();
+        // TODO: Instead of screenscraping, retrieve from JS data model
+        var url = $link.attr('href');
+        var property = getPropertyFromUrl(url);
+        for (var i = 0; i < model.length; ++i) {
+            if (model[i].property === property) return;
+        }
+        model.push({
+            property: property,
+            link: url,
+            caption: $link.text(),
+            type: RuleType.PRESENT,
+            values: null
+        });
+        rerenderUiRules();
+    }
+
+    function getPropertyFromUrl(url) {
+        var colon = url.lastIndexOf(':');
+        return url.substr(colon + 1);
+    }
+
+    function getEntityFromUrl(url) {
+        var slash = url.lastIndexOf('/');
+        return url.substr(slash + 1);
+    }
+
+    function addItemValue() {
+        var $button = $(this);
+        var $entityLink = $button.parent().children('a').first();
+        // TODO: Instead of screenscraping, retrieve from JS data model
+        var entityUrl = $entityLink.attr('href');
+        var entity = getEntityFromUrl(entityUrl);
+        var $propertyLink = $button.closest('.wikibase-statementgroupview').find('.wikibase-statementgroupview-property-label').children('a').first();
+        var propertyUrl = $propertyLink.attr('href');
+        var property = getPropertyFromUrl(propertyUrl);
+        var existingItem = null;
+        for (var i = 0; i < model.length; ++i) {
+            if (model[i].property === property) {
+                existingItem = model[i];
+                break;
+            }
+        }
+        if (existingItem && existingItem.values && existingItem.values.length) {
+            for (var j = 0; j < existingItem.values.length; ++j) {
+                var value = existingItem.values[j];
+                if (value.type ===  value.link === entityUrl) {
+                    return;
+                }
+            }
+        }
+        var newValue = {
+            link: entityUrl,
+            caption: $entityLink.text(),
+            sparql: 'wd:' + entity
+        };
+        if (!existingItem) {
+            model.push({
+                property: property,
+                link: propertyUrl,
+                caption: $propertyLink.text(),
+                type: RuleType.EQUAL,
+                values: [newValue]
+            });
+        } else {
+            existingItem.values = (existingItem.values || []);
+            existingItem.values.push(newValue);
+
+            switch (existingItem.type) {
+                case RuleType.EQUAL:
+                    existingItem.type = RuleType.ALL_OF;
+                    break;
+                case RuleType.NOT_EQUAL:
+                    existingItem.type = RuleType.NONE_OF;
+                    break;
+                case RuleType.PRESENT:
+                    existingItem.type = RuleType.EQUAL;
+                    break;
+                case RuleType.NOT_PRESENT:
+                    existingItem.type = RuleType.NOT_EQUAL;
+                    break;
+            }
+        }
+        rerenderUiRules();
+    }
+
+    function editSparql() {
+        doSparql(false);
+    }
+
+    function executeSearch() {
+        doSparql(true);
+    }
+
+    function doSparql(runQuery) {
+        var positivePatterns = [];
+        var negativePatterns = [];
+        for (var i = 0; i < model.length; ++i) {
+            var item = model[i];
+            switch (item.type) {
+                case RuleType.ALL_OF:
+                    var values = ['\t?item wdt:', item.property, ' '];
+                    for (var k = 0; k < item.values.length; ++k) {
+                        if (k > 0) values.push(', ');
+                        values.push(item.values[j].sparql);
+                    }
+                    positivePatterns.push(values.join(''));
+                    break;
+
+                case RuleType.EQUAL:
+                case RuleType.NOT_EQUAL:
+                case RuleType.ANY_OF:
+                case RuleType.NONE_OF:
+                    var noneOfType = item.type === RuleType.NONE_OF;
+                    var negativeType = noneOfType || item.type === RuleType.NOT_EQUAL;
+                    var cumulativeType = noneOfType || item.type === RuleType.ANY_OF;
+                    var anyOfType = item.type === RuleType.ANY_OF;
+                    var typeWithBlock = negativeType || anyOfType;
+                    var pattern = '';
+                    if (negativeType) pattern += '\tMINUS {\n';
+                    else if (anyOfType) pattern += '\t{\n';
+                    var tabs = typeWithBlock ? '\t\t' : '\t';
+
+                    for (var j = 0; j < item.values.length; ++j) {
+                        pattern += tabs;
+                        if (cumulativeType) {
+                            if (j > 0) pattern += 'OR ';
+                            pattern += '{ ';
+                        }
+                        pattern += '?item wdt:' + item.property + ' ' + item.values[j].sparql;
+                        if (cumulativeType) pattern += ' }\n';
+                        else pattern += ' .\n';
+                    }
+
+                    if (typeWithBlock) pattern += '\t}\n';
+                    (negativeType ? negativePatterns : positivePatterns).push(pattern);
+                    break;
+                case RuleType.PRESENT:
+                    positivePatterns.push('\t?item wdt:' + item.property + ' ?any' + item.property + ' .\n');
+                    break;
+                case RuleType.NOT_PRESENT:
+                    negativePatterns.push('\tMINUS { ?item wdt:' + item.property + ' ?any' + item.property + ' }\n');
+                    break;
+            }
+        }
+
+        // TODO: DISTINCT enable/disable?
+        var sparql =
+            'SELECT DISTINCT ?item ?itemLabel WHERE {\n'
+            + positivePatterns.join('')
+            + negativePatterns.join('')
+            + '\tSERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". ?item rdfs:label ?itemLabel. }\n}\nLIMIT 30';
+
+        var sparqlUrl = runQuery
+            ? 'https://query.wikidata.org/embed.html#' + encodeURI(sparql)
+            : 'https://query.wikidata.org/#' + encodeURI(sparql);
+        window.open(sparqlUrl);
+    }
+
+    function rerenderUiRules() {
+        $uirulelist.html('');
+        for (var i = 0; i < model.length; ++i) {
+            var item = model[i];
+            var $item = $('<li>');
+            $item.data('idx', i);
+            $item.append($('<a>').attr('href', item.link).append($('<i>').text(item.caption)));
+            renderTypeSelector(item, $item);
+            var $removeBtn = $('<a>').text(msgs.remove).click(removeRule);
+            // TODO: "show value in result" switch for NOT_EQUAL, ANY_OF, NONE_OF, PRESENT
+            switch (item.type) {
+                case RuleType.EQUAL:
+                case RuleType.NOT_EQUAL:
+                    renderSingleValue(item.values[0], $item);
+                    $item.append(' ');
+                    $item.append($removeBtn);
+                    break;
+                case RuleType.ANY_OF:
+                case RuleType.ALL_OF:
+                case RuleType.NONE_OF:
+                    $item.append(' ');
+                    $item.append($removeBtn);
+                    var $values = $('<ul>');
+                    $item.append($values);
+                    for (var j = 0; j < item.values.length; ++j) {
+                        var $value = $('<li>');
+                        $value.data('idx', j);
+                        renderSingleValue(item.values[j], $value);
+                        var $valueRemoveBtn = $('<a>').text(msgs.remove).click(removeValue);
+                        $value.append(' ');
+                        $value.append($valueRemoveBtn);
+                        $values.append($value);
+                    }
+                    break;
+                case RuleType.PRESENT:
+                case RuleType.NOT_PRESENT:
+                    $item.append(' ');
+                    $item.append($removeBtn);
+                    break;
+            }
+            $uirulelist.append($item);
+        }
+        var noRules = !model.length;
+        $sparqlbtn.prop('disabled', noRules);
+        $clearbtn.prop('disabled', noRules);
+        validateSearchability();
+    }
+
+    function removeRule() {
+        var $rule = $(this).closest('li');
+        var idx = +$rule.data('idx');
+        model.splice(idx, 1);
+        rerenderUiRules();
+    }
+
+    function removeValue() {
+        var $value = $(this).closest('li');
+        var $rule = $value.parent().closest('li');
+        var valueIdx = +$value.data('idx');
+        var ruleIdx = +$rule.data('idx');
+        var rule = model[ruleIdx];
+        rule.values.splice(valueIdx, 1);
+        switch (rule.values.length) {
+            case 0:
+                rule.type = rule.type === RuleType.EQUAL ? RuleType.PRESENT : RuleType.NOT_PRESENT;
+                break;
+            case 1:
+                rule.type = rule.type === RuleType.NONE_OF ? RuleType.NOT_EQUAL : RuleType.EQUAL;
+                break;
+        }
+        rerenderUiRules();
+    }
+
+    function renderTypeSelector(item, $container) {
+        var $select = $('<select>').data('wd-prop', item.property);
+        switch (item.type) {
+            case RuleType.EQUAL:
+            case RuleType.NOT_EQUAL:
+                renderOption(RuleType.EQUAL, item.type, $select);
+                renderOption(RuleType.NOT_EQUAL, item.type, $select);
+                break;
+            case RuleType.ANY_OF:
+            case RuleType.ALL_OF:
+            case RuleType.NONE_OF:
+                renderOption(RuleType.ANY_OF, item.type, $select);
+                renderOption(RuleType.ALL_OF, item.type, $select);
+                renderOption(RuleType.NONE_OF, item.type, $select);
+                break;
+            case RuleType.PRESENT:
+            case RuleType.NOT_PRESENT:
+                renderOption(RuleType.PRESENT, item.type, $select);
+                renderOption(RuleType.NOT_PRESENT, item.type, $select);
+                break;
+        }
+        $select.change(typeChanged);
+        $container.append(' ');
+        $container.append($select);
+    }
+
+    function typeChanged() {
+        var $select = $(this);
+        var property = $select.data('wd-prop');
+        var selectedType = +$select.val();
+        for (var i = 0; i < model.length; ++i) {
+            var item = model[i];
+            if (item.property === property) {
+                item.type = selectedType;
+                validateSearchability();
+                return;
+            }
+        }
+    }
+
+    function validateSearchability() {
+        var searchableModel = false;
+        for (var i = 0; !searchableModel && i < model.length; ++i) {
+            var item = model[i];
+            switch (item.type) {
+                case RuleType.PRESENT:
+                    if (item.property !== PROP_INSTANCE_OF) searchableModel = true;
+                    break;
+                case RuleType.ALL_OF:
+                case RuleType.ANY_OF:
+                case RuleType.EQUAL:
+                    searchableModel = true;
+                    break;
+                case RuleType.NOT_PRESENT:
+                case RuleType.NONE_OF:
+                case RuleType.NOT_EQUAL:
+                    break;
+            }
+        }
+        $searchbtn.prop('disabled', !searchableModel);
+    }
+
+    function renderOption(ruleType, currentType, $select) {
+        var $option = $('<option>', { value: ruleType }).text(msgs['type' + ruleType]).prop('selected', ruleType === currentType);
+        $select.append($option);
+    }
+
+    function renderSingleValue(value, $container) {
+        if (value.link) {
+            $container.append($('<a>').attr('href', value.link).append($('<i>').text(value.caption)));
+        } else {
+            $container.append($('<i>').text(value.caption));
+        }
+    }
+})(jQuery);
