444aeecd8121055f28eb46f0b27ea8ad1ce0d673
Mormegil <mormegil@centrum.cz>
2019-08-30 14:50:53 +0200

    Fix the stable version as well

--- magic-search/magic-search.js
+++ magic-search/magic-search.js
@@ -1,39 +1,48 @@
 /* global mediaWiki, jQuery */
 
-var wol = window.onload;
-window.onload = function () {
-    if (typeof wol === 'function') wol();
+(function () {
+    var wol;
+    function initMagicSearch() {
+        if (typeof wol === 'function') wol();
 
-    (function (mw, $) {
-        var entityId = mw.config.get('wbEntityId');
-        if (!entityId) {
-            return;
-        }
+        (function (mw, $) {
+            var entityId = mw.config.get('wbEntityId');
+            if (!entityId) {
+                return;
+            }
 
-        mw.loader.using(['mediawiki.util']).then(function () {
-            var portletLink = mw.util.addPortletLink(
-                'p-tb',
-                '#',
-                // TODO: i18n in this loader
-                // msgs.portletlink,
-                'Hledat podle této',
-                'sbt-portletlink',
-                // msgs.portlettooltip
-                'Hledání dalších položek na základě této položky'
-            );
-            var uiboxLoading = false;
-            $(portletLink).click(function (e) {
-                e.preventDefault();
+            mw.loader.using(['mediawiki.util']).then(function () {
+                var portletLink = mw.util.addPortletLink(
+                    'p-tb',
+                    '#',
+                    // TODO: i18n in this loader
+                    // msgs.portletlink,
+                    'Hledat podle této',
+                    'sbt-portletlink',
+                    // msgs.portlettooltip
+                    'Hledání dalších položek na základě této položky'
+                );
+                var uiboxLoading = false;
+                $(portletLink).click(function (e) {
+                    e.preventDefault();
 
-                if (uiboxLoading) return;
-                uiboxLoading = true;
+                    if (uiboxLoading) return;
+                    uiboxLoading = true;
 
-                var $uibox = $('<div id="sbt-uibox">');
-                $uibox.append($('<img src="https://upload.wikimedia.org/wikipedia/commons/f/f8/Ajax-loader%282%29.gif" />'));
-                $(document.body).append($uibox);
+                    var $uibox = $('<div id="sbt-uibox">');
+                    $uibox.append($('<img src="https://upload.wikimedia.org/wikipedia/commons/f/f8/Ajax-loader%282%29.gif" />'));
+                    $(document.body).append($uibox);
 
-                mw.loader.load('https://cdn.rawgit.com/mormegil-cz/wikidata-tools/0.1/magic-search/magic-search-main.js');
+                    mw.loader.load('https://cdn.rawgit.com/mormegil-cz/wikidata-tools/0.1/magic-search/magic-search-main.js');
+                });
             });
-        });
-    })(mediaWiki, jQuery);
-};
+        })(mediaWiki, jQuery);
+    };
+    if (document.readyState === 'complete') {
+        wol = null;
+        initMagicSearch();
+    } else {
+        wol = window.onload;
+        window.onload = initMagicSearch;
+    }
+})();
