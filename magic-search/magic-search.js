40b4aacd60158c1a51dc6a09e2ca3a373f3631ac
Mormegil <mormegil@centrum.cz>
2023-08-09 12:12:25 +0200

    Fix magic-search
    Replace rawgit.com which shouldn’t have been for a long time already.

--- magic-search/magic-search.js
+++ magic-search/magic-search.js
@@ -33,7 +33,7 @@
                     $uibox.append($('<img src="https://upload.wikimedia.org/wikipedia/commons/f/f8/Ajax-loader%282%29.gif" />'));
                     $(document.body).append($uibox);
 
-                    mw.loader.load('https://cdn.rawgit.com/mormegil-cz/wikidata-tools/0.1/magic-search/magic-search-main.js');
+                    mw.loader.load('https://mormegil-cz.github.io/wikidata-tools/magic-search/magic-search-main.js');
                 });
             });
         })(mediaWiki, jQuery);
