5947042983a4703799abf9f21d41f4dd448cd170
Mormegil <mormegil@centrum.cz>
2024-01-05 11:12:54 +0100

    Update property data
    + error check

--- third-party-url/process-propData.js
+++ third-party-url/process-propData.js
@@ -8,6 +8,10 @@ for (let i = 0; i < queryResults.length; ++i) {
 	let p = row.p.value;
     let list = results[p] || [];
     results[p] = list;
+    if (!row.c) {
+        console.error('Missing c', row);
+        throw 'Missing c';
+    }
     list.push({u: row.u.value, c: row.c.value, r: row.r && row.r.value});
 }
 
