7b10c7ec4624753b6284b92eb0464946ea768102
Mormegil <mormegil@centrum.cz>
2026-09-21 09:42:00 +0200

    Update third-party-url property data
    + resilience against novalue formatter claims

--- third-party-url/process-propData.js
+++ third-party-url/process-propData.js
@@ -9,6 +9,10 @@ for (let i = 0; i < queryResults.length; ++i) {
     let list = results[p] || [];
     results[p] = list;
     if (!row.c) {
+        if (row.u.type === 'uri' && row.u.value && row.u.value.startsWith('http://www.wikidata.org/.well-known/genid/')) {
+            console.warn('Novalue formatter claim', row);
+            continue;
+        }
         console.error('Missing c', row);
         throw 'Missing c';
     }
